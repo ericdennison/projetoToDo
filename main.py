600b1dda35d2689da71422cb86f1205cbd802996
eddesks <ericdenniso@gmail.com>
2024-10-21 16:56:15 -0300

    terminado linha Scrollbar adicionado função adicionar tarefas

--- main.py
+++ main.py
@@ -9,12 +9,33 @@ janela.title("Meu app de Tarefas")
 janela.configure(bg='#F0F0F0')
 janela.geometry('550x600')
 
+frame_em_edicao = None
+
+# Função Adicionar Tarefa
+def adicionar_tarefa():
+    global frame_em_edicao
+    
+    tarefa = entrada_tarefa.get().strip()
+    if tarefa and tarefa != "Escreva sua tarefa aqui":
+        if frame_em_edicao is not None:
+            atualizar_tarefa(tarefa)
+            frame_em_edicao = None
+        else:
+            adicionar_item_tarefa(tarefa)
+            entrada_tarefa.delete(0, tk.END)
+    else:
+        messagebox.showwarning("Entrada Inválida", "Por favor Insira uma tarefa!")
+
+
+
+
 # Cabeçalho
 fonte_cabecalho = font.Font(family='Garamond', size=24, weight='bold')
 rotulo_cabecalho = tk.Label(janela,text="Meu App de Tarefas", font=fonte_cabecalho, bg='#F0F0F0', fg='#333').pack(pady=20,)
 
 # Container
-frame = tk.Frame(janela, bg='#F0F0F0').pack(pady=10)
+frame = tk.Frame(janela, bg='#F0F0F0')
+frame.pack(pady=10)
 
 # Campo Entrada Texto
 entrada_tarefa = tk.Entry(frame, font=('Garamond', 14), relief=tk.FLAT, bg='white', fg='grey', width=30)
@@ -38,6 +59,9 @@ scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 canvas.configure(yscrollcommand=scrollbar.set)
 canvas_interior = tk.Frame(canvas, bg='white')
 canvas.create_window((0,0), window=canvas_interior, anchor='nw')
+canvas_interior.bind('<Configure>',lambda e: canvas.configure(scrollregion=canvas.bbox('all')))
+
+
 
 janela.mainloop()
 # JANELA fim
